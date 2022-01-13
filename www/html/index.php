97179821c57b7ca09f6cdac308837c72fa608eee
karen <caremayo812@icloud.com>
2022-01-14 00:42:31 +0900

    うわあ

---
+++ www/html/index.php
@@ -0,0 +1,22 @@
+<?php
+// phpinfo();
+
+$dsn = 'mysql:dbname=quizy_db;charset=utf8;host=db';
+$user = 'karen';
+$password = 'password';
+
+try {
+    $dbh = new PDO($dsn, $user, $password);
+    // $dbh = new PDO(
+    //     'mysql:host=db;dbname=quizy;charset=utf8mb4',
+    //     'naoki',
+    //     'karen',
+    //     'password'
+    // );
+    echo "接続成功\n";
+} catch (PDOException $e) {
+    echo "接続失敗: " . $e->getMessage() . "\n";
+    exit();
+}
+
+?>
