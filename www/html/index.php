dd422722bdf5d9281e9cd3b3e386953564ee7056
karen <caremayo812@icloud.com>
2022-01-28 23:50:21 +0900

    reireiからもらったやつ貼った

--- www/html/index.php
+++ www/html/index.php
@@ -1,4 +1,39 @@
 <?php
+
+// (1) 取得するデータのidを指定
+$id = $_REQUEST['id'];
+
+// (2) データベースに接続
+$pdo = new PDO('mysql:charset=UTF8;dbname=quizy;host=db', 'root', 'root');
+
+// (3) SQL作成
+$stmt = $pdo->prepare("SELECT * FROM choices WHERE id = :id");
+
+// (4) 登録するデータをセット
+$stmt->bindParam( ':id', $id, PDO::PARAM_INT);
+
+// (5) SQL実行
+$res = $stmt->execute();
+
+// (6) 該当するデータを取得
+if( $res ) {
+	$data = $stmt->fetch();
+	// var_dump($data);
+    echo $data['name'];
+
+}
+
+// (7) データベースの接続解除
+$pdo = null;
+
+?>
+
+
+<!-- ________________________________________ -->
+
+
+
+<?php
 // phpinfo();
 
 $dsn = 'mysql:dbname=quizy_db;charset=utf8;host=db';
