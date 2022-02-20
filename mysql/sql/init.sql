9318b693f900e1572d5905b2f0b01dca5c0f069a
karen <caremayo812@icloud.com>
2022-02-20 18:16:51 +0900

    urlからid取得して見えるページの分岐。posse課題、終わる希望が見えてきた・・・！✨

--- mysql/sql/init.sql
+++ mysql/sql/init.sql
@@ -1,2 +1,9 @@
 USE mysql;
 ALTER user 'karen'@'%' identified WITH mysql_native_password BY 'password';
+
+USE quizydb;
+DROP TABLE IF EXISTS big_questions; 
+-- 上かくといいらしい
+CREATE TABLE big_questions (id int, name varchar(255));
+INSERT INTO big_questions (id, name) VALUES (1,'tokyo');
+INSERT INTO big_questions (id, name) VALUES (2,'hiroshima');
